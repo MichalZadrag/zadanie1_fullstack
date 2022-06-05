e5519c16dde70c6db79e4c35a2af5a8623298031
Michal Zadrag <Michal.Zadrag@atende.pl>
2022-06-05 19:58:24 +0200

    add a few changes to dockerfiles

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const express = require("express");
 const app = express();
 app.use(cors());
 
-app.listen(3000, () => {
-  console.log("Application started and Listening on port 3000");
-});
+app.listen(3000, () => {});
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
